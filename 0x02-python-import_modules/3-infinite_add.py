98861fed227c63da1f821239fa50657e68806aab
peternmacharia <peternmacharia@outlook.com>
2022-09-23 01:49:20 +0300

    Commit Task #3 #1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "_main__":
+
+if __name__ == "__main__":
     """Prints the addition of all arguments."""
     import sys
+
     total = 0
     for i in range(len(sys.argv) -1):
         total += int(sys.argv[i + 1])
