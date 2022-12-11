9b7979fc9a781f3a3b1507304941147928b9df49
peternmacharia <peternmacharia@outlook.com>
2022-12-11 21:21:59 +0300

    Task #6#7#8 commit

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- A script that lists all row of the table from a database in MySQL server
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/7-insert_values.sql
@@ -0,0 +1,2 @@
+-- A script that inserts a new row in the table in MySQL server
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
