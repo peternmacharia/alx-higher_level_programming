01455c042ae6c29501e8cd116d76a95a2e814c80
peternmacharia <peternmacharia@outlook.com>
2022-12-16 16:00:37 +0300

    Task #7 commit

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- a script that creates database hbtn_0d_usa and table cities in MySQL server
+-- create database and check if it exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- select and use the created database
+USE hbtn_0d_usa;
+-- create table with description
+CREATE TABLE IF NOT EXISTS cities 
+(id INT UNIQUE AUTO_GENERATE NOT NULL, 
+state_id INT NOT NULL, 
+name VARCHAR(256) NOT NULL, 
+PRIMARY KEY(id), 
+FOREIGN KEY(state_id) REFERENCES states(id));
