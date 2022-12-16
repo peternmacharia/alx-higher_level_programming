9f3efa1a8ad4e7ab3103d7dd20bfb898b7f38366
peternmacharia <peternmacharia@outlook.com>
2022-12-16 15:16:09 +0300

    Task #2 commit #3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
--- A script that creates database hbtn_0d_2 and user user_0d_2
--- Creates database and checks if it exists
+-- a script that creates database hbtn_0d_2 and user user_0d_2
+-- creates database and checks if it exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Creates the user_0d_2 with password user_0d_2_pwd
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY `user_0d_2_pwd`;
--- Grants SELECT privilege to the user
+-- creates the user_0d_2 with password user_0d_2_pwd
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privilege to the user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
