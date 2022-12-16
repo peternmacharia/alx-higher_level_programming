7cda43269c1cd366d0c5f3b9158129afce3fc263
peternmacharia <peternmacharia@outlook.com>
2022-12-16 15:26:05 +0300

    Task #4 commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- a script that creates the table id_not_null on MySQL server
+-- create table and checks if it already exists
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT=1, name VARCHAR(256));
